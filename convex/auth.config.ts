722c0e349be7fc13765032f9ab3b4a38db31bec1
minhlip <minhlips2002@gmail.com>
2024-02-26 22:32:05 +0700

    Feat:setup convex and clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: "https://up-coyote-0.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
