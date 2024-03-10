ee0b09fb61b46efa71139d86106906c1c5f93082
minhlip <minhlips2002@gmail.com>
2024-03-10 17:17:32 +0700

    Feat: Update sidebar item

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,24 +1,69 @@
 'use client';
 
-import { LucideIcon } from 'lucide-react';
+import { Id } from '@/convex/_generated/dataModel';
+import { cn } from '@/lib/utils';
+import { ChevronDown, ChevronRight, LucideIcon } from 'lucide-react';
 
 interface ItemProps {
+  id?: Id<'documents'>;
+  documentsIcon?: string;
+  active?: boolean;
+  expanded?: boolean;
+  isSearch?: boolean;
+  onExpand?: () => void;
+  level?: number;
   onClick: () => void;
   label: string;
   icon: LucideIcon;
 }
 
 export const Item = (props: ItemProps) => {
-  const { onClick, label, icon: Icon } = props;
+  const {
+    id,
+    onClick,
+    label,
+    icon: Icon,
+    active,
+    documentsIcon,
+    isSearch,
+    level = 0,
+    onExpand,
+    expanded,
+  } = props;
+
+  const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+
   return (
     <div
       onClick={onClick}
       role="button"
-      style={{ paddingLeft: '12px' }}
-      className="group min-h-[27px] text-base py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium "
+      style={{ paddingLeft: level ? `${level * 12 + 12}px` : '12px' }}
+      className={cn(
+        'group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium',
+        active && 'bg-primary/5 text-primary'
+      )}
     >
-      <Icon className="w-[18px] h-[18px] shrink-0 mr-2 text-muted-foreground" />
+      {!!id && (
+        <div
+          role="button"
+          className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
+          onClick={() => {}}
+        >
+          <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground" />
+        </div>
+      )}
+
+      {documentsIcon ? (
+        <div className="shrink-0 mr-2 text-[18px]">{documentsIcon}</div>
+      ) : (
+        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      )}
       <span className="truncate">{label}</span>
+      {isSearch && (
+        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center justify-center gap-1 rounded border bg-muted px-1.5 font-mono text-[12px] font-medium text-muted-foreground opacity-100">
+          <span className="text-xs">Ctrl K</span>
+        </kbd>
+      )}
     </div>
   );
 };
