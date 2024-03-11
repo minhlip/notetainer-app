d251a65cfc4a8ad4192967bafdb5640ab67df517
minhlip <minhlips2002@gmail.com>
2024-03-11 14:11:52 +0700

    Feat: Document List, Children Document, update UI of sidebar item

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -15,12 +15,12 @@ import { useMutation, useQuery } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 import { Item } from './item';
 import { toast } from 'sonner';
+import DocumentList from './document-list';
 
 function Navigation() {
   const pathname = usePathname();
   const isMobile = useMediaQuery('(max-width: 768px)');
 
-  const documents = useQuery(api.documents.get);
   const create = useMutation(api.documents.create);
 
   const handleCreate = () => {
@@ -144,9 +144,7 @@ function Navigation() {
         </div>
 
         <div className="mt-4">
-          {documents?.map((document) => (
-            <p key={document._id}>{document.title}</p>
-          ))}
+          <DocumentList />
         </div>
 
         <div
